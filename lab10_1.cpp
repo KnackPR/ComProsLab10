a62a7a7192c48bb9f9ced052bb358bfcc781d57c
KnackPR <peerawich_r@cmu.ac.th>
2022-12-27 16:13:26 +0700

    ทำไม float ไม่ได้อ่ะครับ

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,10 +2,16 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+    double loan , rate , ppy ;
+    
 	cout << "Enter initial loan: ";
+	cin >> loan ;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate ;
 	cout << "Enter amount you can pay per year: ";
+	cin >> ppy ; 
+
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +26,34 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	cout << fixed << setprecision(2);
+	int i = 1 ;
+	double pb , inte , total , nb ;
+	pb = loan ; 
+	inte = loan * (rate/100);
+	total = pb + inte ;
+	nb = total - ppy ;
+	while( nb > 0 && ppy < loan ){
+	cout << setw(13) << left << i ; 
+	cout << setw(13) << left << pb ; 
+	cout << setw(13) << left << inte ; 
+	cout << setw(13) << left << total ;
+	cout << setw(13) << left << ppy ;
+	cout << setw(13) << left << nb ;
+	i++ ; 
+	pb = nb ;
+	inte = pb * (rate/100) ;
+	total = pb + inte ; 
+	nb = total - ppy ;
+	cout << "\n";
+	}
+	cout << setw(13) << left << i ; 
+	cout << setw(13) << left << pb ; 
+	cout << setw(13) << left << inte ; 
+	cout << setw(13) << left << total ;
+	ppy = total ;
+	cout << setw(13) << left << ppy ;
+	nb = total - ppy ;
+	cout << setw(13) << left << nb ;
 	return 0;
 }
